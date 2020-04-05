57d7259a3ad59989f41f84bcbf69bc39781c2e45
Felipe <felipe.pereira.dev@gmail.com>
2020-04-05 10:15:41 -0300

    Updates

---
+++ precache-manifest.d24e1d87cb17f2cb3573eb3f24c2d894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56619db12753191bc6f5601e758e936",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "473d084dde6a90fabe92",
+    "url": "/tic-tac-toe/static/css/main.b0e1ba72.chunk.css"
+  },
+  {
+    "revision": "244cc5651cd6a964d55a",
+    "url": "/tic-tac-toe/static/js/2.661c19bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.661c19bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473d084dde6a90fabe92",
+    "url": "/tic-tac-toe/static/js/main.a599c1f3.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
